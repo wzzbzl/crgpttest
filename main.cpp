38b846baafa9cfec04dcf435603f2ef925b38cb1
wzzbzl <128467103+wzzbzl@users.noreply.github.com>
2023-03-23 15:12:48 +0800

    add febonachi function.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
 
-void funcTest_1(){
+int Fib(int i)
+{
+    if (0 == i)
+        return 0;
+    else if (1 == i)
+        return 1;
+    else
+        return Fib(i - 1) + Fib(i - 2);
+}
+
+void funcTest_1()
+{
     int testPara = 0;
 }
 
@@ -9,6 +20,8 @@ int main()
     std::cout << "hello world!" << std::endl;
 
     funcTest_1();
+
+    std::cout << "fibonacci 15 is: " << Fib(15) << std::endl;
 
     return 0;
 }
